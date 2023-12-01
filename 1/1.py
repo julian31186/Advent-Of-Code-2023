2cf0834d043b666458cdc2152e8ed9eb4ed50dcf
Julian Elmasry <julian31186@gmail.com>
2023-12-01 17:35:49 -0500

    p2 annoying

--- 1/1.py
+++ 1/1.py
@@ -23,11 +23,35 @@ def one():
     return res
 
 def two():
-    import math
     digits = {"one" : 1,"two" : 2,"three" : 3,"four" : 4,"five" : 5,"six" : 6,"seven" : 7,"eight" : 8,"nine" : 9}
     res = 0
     
+    for row in arr:
+        s = ""
+        for i in range(len(row)):
+            if row[i].isnumeric(): s += row[i]
+            elif row[i:i + 3] in digits.keys():
+                s += str(digits[row[i:i + 3]])
+            elif row[i:i + 4] in digits.keys():
+                s += str(digits[row[i:i + 4]])
+            elif row[i:i + 5] in digits.keys():
+                s += str(digits[row[i:i + 5]])
     
+        
+        curr = ""
+        for l in range(len(s)):
+            if s[l].isnumeric():
+                curr += s[l]
+                break
+        for r in range(len(s) - 1, -1, -1):
+            if s[r].isnumeric():
+                curr += s[r]
+                break
+        print(curr)
+        res += int(curr)
+        
+
+    print(res)
     return res
     
     
